fbbd8001c10502e4288a8ce7438387a4cea6a252
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-04-20 03:35:40 +0200

    CA

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # Puppet manifest to kill a process named killmenow
-
-exec { 'kill_killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  onlyif      => 'pgrep -f killmenow',
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
