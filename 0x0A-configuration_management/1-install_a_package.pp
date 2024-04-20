a8ab7bb6e8c2713bb1fe3a32e742cf9a03dcd61f
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-04-20 03:22:36 +0200

    CA

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
-# Puppet manifest to install Flask from pip3
+# Puppet manifest to install puppet
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin', '/usr/local/bin'],
-  refreshonly => true,
-}
-
-# Ensure the package is present after installation
-package { 'python3-flask':
-  ensure => '2.1.0',
-  require => Exec['install_flask'],
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
