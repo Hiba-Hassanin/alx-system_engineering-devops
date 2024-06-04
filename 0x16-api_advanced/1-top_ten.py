26fc7105a750bd2091c8e7e029417f2004d177a0
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 01:00:43 +0200

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,18 +3,15 @@
 import requests
 import sys
 
+
 def top_ten(subreddit):
-    """
-    Prints the top ten post titles
-    from a given subreddit.
-    If the subreddit is invalid
-    prints None.
-    """
+    """ Returns: top ten post titles
+        or None if queried subreddit is invalid """
     headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
-
-    response = requests.get(url, headers=headers, allow_redirects=False, params=parameters)
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
 
     if response.status_code == 200:
         titles_ = response.json().get('data').get('children')
