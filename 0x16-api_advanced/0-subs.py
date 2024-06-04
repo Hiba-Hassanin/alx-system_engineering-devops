4515008dcfc0d29be601e81baf738c08990752ff
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 00:33:45 +0200

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number
-of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given,
-the function should return 0.
+Queries the Reddit API and returns the
+number of subscribers for a given subreddit.
+If an invalid subreddit is provided,
+the function returns 0.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
+    """Construct the URL for the Reddit API endpoint."""
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
     headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
+    """Check if the request was successful."""
     if response.status_code != 200:
         return (0)
     response = response.json()
