e83ba8999438da8fa05ce076a8907f23260b4cb9
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 01:07:18 +0200

    API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
+""" Recursive function that queries the Reddit API for hot articles """
 import requests
 
-def recurse(subreddit, hot_list=[]):
-    """
-    Returns a list containing the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit, the function returns None.
-    """
-    url = f"https://api.reddit.com/r/{subreddit}/hot"
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+def recurse(subreddit, hot_list=[], after=None):
+    """ Returns a list of titles of all hot posts in a subreddit """
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 100, 'after': after}
 
-    if response.status_code != 200:
-        return None
+    response = requests.get(url, headers=headers, allow_redirects=False, params=parameters)
 
-    data = response.json().get('data')
-    children = data.get('children')
-    hot_list.extend([child.get('data').get('title') for child in children])
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        children = data.get('children', [])
+        for child in children:
+            hot_list.append(child.get('data', {}).get('title', ''))
 
-    after = data.get('after')
-    if after:
-        return recurse(subreddit, hot_list)
-    else:
+        after = data.get('after', None)
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
         return hot_list
+    else:
+        return None
