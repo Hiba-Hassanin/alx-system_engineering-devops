2f547e4ff5f1cb39e8ba1f89aca0028d5a03d147
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-04 23:58:09 +0200

    API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If an invalid subreddit is given, the function returns 0.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    try:
+        # Set the user agent to avoid rate limiting
+        headers = {'User-Agent': 'my_app/0.0.1'}
+
+        # Send a GET request to the Reddit API
+        response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Get the number of subscribers from the response
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except:
+        return 0
