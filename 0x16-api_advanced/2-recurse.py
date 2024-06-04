540fa67dc83ac9a14deddaf857cb54f8ce855b32
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 01:06:23 +0200

    API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+import requests
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Returns a list containing the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit, the function returns None.
+    """
+    url = f"https://api.reddit.com/r/{subreddit}/hot"
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get('data')
+    children = data.get('children')
+    hot_list.extend([child.get('data').get('title') for child in children])
+
+    after = data.get('after')
+    if after:
+        return recurse(subreddit, hot_list)
+    else:
+        return hot_list
