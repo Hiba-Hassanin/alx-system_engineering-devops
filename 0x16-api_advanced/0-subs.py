15f3db2fd16f6ccebbde515082784f309217e1fb
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 00:21:52 +0200

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,20 +3,20 @@
 Queries the Reddit API and returns the number of total subscribers for a given
 subreddit.
 """
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
-    else:
-        return (0)
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
