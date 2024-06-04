27a98c36cb0caa17b024c4fcb85db4fa59b41a3f
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 00:38:37 +0200

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+"""
+This script queries the Reddit API and prints
+the titles of the first 10 hot posts
+for a given subreddit. If the subreddit
+is not valid, it prints 'None'.
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints
+    the titles of the first 10 hot
+    posts for the given subreddit.
+
+    Args:
+        subreddit (str): The name of
+               the subreddit to search.
+    """
+    url = f"https://api.reddit.com/r/{subreddit}/hot"
+    headers = {'User-Agent': 'CustomClient/1.0'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        """
+        Check if the request was successful. 
+        If not, print 'None' and return.
+        """
+        if response.status_code != 200:
+            print("None")
+            return
+
+        """
+        Extract the titles of the first 10 
+        hot posts and print them.
+        """
+        data = response.json().get("data", {}).get("children", [])
+        for post in data[:10]:
+            print(post["data"]["title"])
+
+    except requests.exceptions.RequestException as e:
+        print("None")
