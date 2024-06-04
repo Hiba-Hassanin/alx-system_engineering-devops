902cbda2a48f2e73b426d62b5ece94c42987dfef
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 00:26:56 +0200

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If an invalid subreddit is provided, the function returns 0.
 """
-from requests import get
-
+import requests
 
 def number_of_subscribers(subreddit):
-    """ function to get subscriber count"""
-    if subreddit and type(subreddit) is str:
-        subscribers = 0
-        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        req = get(url, headers=headers)
-        if req.status_code == 200:
-            data = req.json()
-            subscribers = data.get('data', {}).get('subscribers', 0)
-        return subscribers
+
+    # Construct the URL for the Reddit API endpoint.
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful.
+    if response.status_code != 200:
+        return 0
+
+    # Convert the response to JSON format
+    response_data = response.json()
+    # Check if the 'data' key is present in the response
+    if 'data' in response_data:
+        return response_data.get('data').get('subscribers')
+    else:
+        return 0
