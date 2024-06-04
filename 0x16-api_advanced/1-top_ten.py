97dc3a06f37cff692a14edb5a9b98a6301468c42
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-05 00:52:17 +0200

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-function that queries the Reddit API and
-prints the titles of the first 10 hot posts
-listed for a given subreddit.
-"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    if subreddit is None or type(subreddit) is not str:
-        print('None')
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-
-    base_url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced_project'}
-    r = requests.get(base_url, headers=headers, allow_redirects=False).json()
-
-    if r.get('data') is None:
-        print('None')
-        return
-
-    i = 0
-    data = r.get('data').get('children')
-
-    for i in range(0, 10):
-        print(data[i].get('data').get('title'))
-        i += 1
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
