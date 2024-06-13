f6f889ce6c84c257890fc3b25ace789438bf63e2
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-14 00:40:08 +0200

    Debug4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,17 @@
-# Increases the amount of traffic an Nginx server
-# can handle by increasing the ULIMIT.
+# Fix Nginx limits by increasing the ULIMIT and restarting the service
 
 # Increase the ULIMIT in the default Nginx configuration file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin'],
+exec { 'increase_nginx_ulimit':
+  command => '/usr/bin/env sed -i "s/15/2000/" /etc/default/nginx',
+  path    => ['/usr/bin', '/bin'],
   onlyif  => 'grep -q "15" /etc/default/nginx',  # Only run if "15" is found in the file
 } ->
 
 # Restart Nginx to apply the new ULIMIT
-exec { 'nginx-restart':
-  command => 'service nginx restart',
+exec { 'restart_nginx':
+  command => '/usr/bin/env service nginx restart',
   path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
   refreshonly => true,  # Only restart if the previous exec ran
-  subscribe   => Exec['fix--for-nginx'],  # Ensure this runs after the previous exec
+  subscribe   => Exec['increase_nginx_ulimit'],  # Ensure this runs after the previous exec
 }
+
