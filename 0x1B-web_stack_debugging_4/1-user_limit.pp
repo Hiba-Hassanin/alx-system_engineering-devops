af85b73ae024b5ca04b82f3bec040d7d0bd52ef1
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-06-14 00:43:04 +0200

    Debug4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,16 +1,3 @@
 # Fix Nginx limits by increasing the ULIMIT and restarting the service
 
-# Increase the ULIMIT in the default Nginx configuration file
-exec { 'increase_nginx_ulimit':
-  command => '/usr/bin/env sed -i "s/15/2000/" /etc/default/nginx',
-  path    => ['/usr/bin', '/bin'],
-  onlyif  => 'grep -q "15" /etc/default/nginx',  # Only run if "15" is found in the file
-} ->
-
-# Restart Nginx to apply the new ULIMIT
-exec { 'restart_nginx':
-  command => '/usr/bin/env service nginx restart',
-  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  refreshonly => true,  # Only restart if the previous exec ran
-  subscribe   => Exec['increase_nginx_ulimit'],  # Ensure this runs after the previous exec
-}
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
